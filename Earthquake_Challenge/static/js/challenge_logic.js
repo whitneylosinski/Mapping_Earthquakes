8f661cdcec529ddb6a3f8c2d3cb15f04fd0ea410
whitney.losinski@gmail.com <[whitney.losinski@gmail.com]>
2020-12-06 15:00:04 -0600

    Completed Challenge Deliverable 3

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -15,6 +15,13 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	accessToken: API_KEY
 });
 
+// We create the third tile layer that will be the background of our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
 	center: [40.7, -94.5],
@@ -25,7 +32,8 @@ let map = L.map('mapid', {
 // Create a base layer that holds all three maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite": satelliteStreets
+  "Satellite": satelliteStreets,
+  "Dark": dark
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
@@ -112,22 +120,24 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   allEarthquakes.addTo(map);
 
 
-// 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
-  // 4. Use the same style as the earthquake data.
-  // function styleInfo(feature) {
-  //   return {
-  //     opacity: 1,
-  //     fillOpacity: 1,
-  //     fillColor: getColor(feature.properties.mag),
-  //     color: "#000000",
-  //     radius: getRadius(feature.properties.mag),
-  //     stroke: true,
-  //     weight: 0.5
-  //   };
-  // }
-  // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
-  // This function determines the color of the marker based on the magnitude of the earthquake.
+  // 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
+    //---------------------------------------------------------------------------------------------------------------------------- 
+    // 4. Use the same style as the earthquake data.
+    // function styleInfo(feature) {
+    //   return {
+    //     opacity: 1,
+    //     fillOpacity: 1,
+    //     fillColor: getColor(feature.properties.mag),
+    //     color: "#000000",
+    //     radius: getRadius(feature.properties.mag),
+    //     stroke: true,
+    //     weight: 0.5
+    //   };
+    // }
+    //----------------------------------------------------------------------------------------------------------------------------
+    // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+    // This function determines the color of the marker based on the magnitude of the earthquake.
     function getColor(magnitude) {
       if (magnitude > 5) {
         return "#ea2c2c";
@@ -139,6 +149,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
         return "#d4ee00";
       }
     }
+    //----------------------------------------------------------------------------------------------------------------------------
     // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
     // function getRadius(magnitude) {
     //   if (magnitude === 0) {
@@ -146,6 +157,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
     //   }
     //   return magnitude * 4;
     // }
+    //----------------------------------------------------------------------------------------------------------------------------
     // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
     // sets the style of the circle, and displays the magnitude and location of the earthquake
     //  after the marker has been created and styled.
